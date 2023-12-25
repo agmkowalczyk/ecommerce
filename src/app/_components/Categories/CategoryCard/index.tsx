ca56e9cab6cd9bd7121b801b0c9f5eb0ca3d760f
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-12-25 14:35:28 +0100

    Add FilterProvider

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+'use client'
 
 import { Category, Media } from '../../../../payload/payload-types'
 import classes from './index.module.scss'
 import Link from 'next/link'
+import { useFilter } from '../../../_providers/Filter'
 
 const CategoryCard = ({ category }: { category: Category }) => {
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
-    <Link href="products" className={classes.card} style={{ backgroundImage: `url(${media.url})` }}>
+    <Link
+      href="products"
+      className={classes.card}
+      style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
+    >
       <p className={classes.title}>{category.title}</p>
     </Link>
   )
